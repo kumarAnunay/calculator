bb3d5d0d771ac1865a31ec901135c811d6d90d96
kumarAnunay <kumaranunay12@gmail.com>
2023-04-22 16:05:49 +0530

    Add

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
-var input = document.getElementById("inputValue");
-var btn = document.querySelectorAll("button");
+
+const input = document.getElementById("inputValue");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const inputValue = input.value;
+        const buttonText = button.querySelector("span").textContent;
+
+        switch (buttonText) {
+            case "C":
+                input.value = "";
+                break;
+            case "←":
+                input.value = inputValue.slice(0, -1);
+                break;
+            case "x":
+                input.value += "*";
+                break;
+            case "/":
+                input.value += "/";
+                break;
+            case "+":
+                input.value += "+";
+                break;
+            case "-":
+                input.value += "-";
+                break;
+            case ".":
+                input.value += ".";
+                break;
+            case "=":
+                try {
+                    if (input.value != "") {
+                        input.value = eval(inputValue);
+                    }
+                } catch (error) {
+                    input.value = "Error";
+                }
+                break;
+            default:
+                input.value += buttonText;
+        }
+    });
+});
